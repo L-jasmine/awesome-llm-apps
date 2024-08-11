ba7b2a3279bf15bb69ef2a7e7e5ce9583693bd51
csh <458761603@qq.com>
2024-08-12 00:40:16 +0800

    use LlamaEdge

--- local_llama3.1_tool_use/llama3_tool_use.py
+++ local_llama3.1_tool_use/llama3_tool_use.py
@@ -1,35 +1,39 @@
 import streamlit as st
 import os
 from phi.assistant import Assistant
-from phi.llm.ollama import Ollama
+from phi.llm.openai import OpenAILike
+
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.serpapi_tools import SerpApiTools
 
 st.set_page_config(page_title="Llama-3 Tool Use", page_icon="🦙")
 
 # Ensure SERPAPI_API_KEY is set
-if 'SERPAPI_API_KEY' not in os.environ:
+if "SERPAPI_API_KEY" not in os.environ:
     st.error("Please set the SERPAPI_API_KEY environment variable.")
     st.stop()
+
 
 def get_assistant(tools):
     return Assistant(
         name="llama3_assistant",
-        llm=Ollama(model="llama3"),
+        llm=OpenAILike(model="llama",base_url="https://llama.gaianet.network/v1"),
         tools=tools,
         description="You are a helpful assistant that can access specific tools based on user selection.",
         show_tool_calls=True,
         debug_mode=True,
         # This setting adds the current datetime to the instructions
         add_datetime_to_instructions=True,
-
     )
 
+
 st.title("🦙 Local Llama-3 Tool Use")
-st.markdown("""
-This app demonstrates function calling with the local Llama3 model using Ollama.
+st.markdown(
+    """
+This app demonstrates function calling with the local Llama3 model using LlamaEdge.
 Select tools in the sidebar and ask relevant questions!
-""")
+"""
+)
 
 # Sidebar for tool selection
 st.sidebar.title("Tool Selection")
@@ -73,18 +77,22 @@ if prompt := st.chat_input("Ask a question based on the enabled tools"):
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 # Sidebar instructions
-st.sidebar.markdown("""
+st.sidebar.markdown(
+    """
 ### How to use:
 1. Select the tools you want to use in the sidebar
 2. Ask questions related to the enabled tools
 3. The assistant will use only the selected tools to answe
 ### Note:
 Make sure you have set the SERPAPI_API_KEY environment variable to use the SerpAPI tool.
-""")
+"""
+)
 
-st.sidebar.markdown("""
+st.sidebar.markdown(
+    """
 ### Sample questions:
 - YFinance: "What's the current price of AAPL?"
 - SerpAPI: "What are the latest developments in AI?"
 - Both: "Compare TSLA stock price with recent news about Tesla's performance"
-""")
+"""
+)
